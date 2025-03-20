e782b0b8e0ab9ce0b350a8a943f06ef8ffc58817
sumbat-ssvlabs <sumbat@ssvlabs.io>
2025-03-20 16:25:06 +0200

    feat: added hoodi

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,8 +1,27 @@
 import type { Address } from 'abitype'
+import { defineChain } from 'viem'
 import { holesky, mainnet } from 'viem/chains'
+
+export const hoodi = defineChain({
+  id: 560048,
+  name: 'Hoodi',
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.hoodi.ethpandaops.io'],
+    },
+  },
+  nativeCurrency: {
+    name: 'Hoodi Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  testnet: true,
+})
+
 export const chains = {
   mainnet,
   holesky,
+  hoodi,
 } as const
 
 export const chainIds = Object.values(chains).map((chain) => chain.id)
@@ -12,11 +31,14 @@ export type SupportedChainsIDs = (typeof chainIds)[number]
 export const graph_endpoints: Record<SupportedChainsIDs, string> = {
   [mainnet.id]: 'https://api.studio.thegraph.com/query/71118/ssv-network-ethereum/version/latest',
   [holesky.id]: 'https://api.studio.thegraph.com/query/71118/ssv-network-holesky/version/latest',
+  [hoodi.id]:
+    'https://graph-node-hoodi.stage.ops.ssvlabsinternal.com/subgraphs/name/ssv-bapps-hoodi/graphql',
 }
 
 export const rest_endpoints: Record<SupportedChainsIDs, string> = {
   [mainnet.id]: 'https://api.ssv.network/api/v4/mainnet',
   [holesky.id]: 'https://api.ssv.network/api/v4/holesky',
+  [hoodi.id]: 'https://api.stage.ops.ssvlabsinternal.com/api/v4/hoodi',
 }
 
 export type ContractAddresses = {
@@ -24,6 +46,7 @@ export type ContractAddresses = {
   getter: Address
   token: Address
 }
+
 export const contracts: Record<SupportedChainsIDs, ContractAddresses> = {
   [mainnet.id]: {
     setter: '0xDD9BC35aE942eF0cFa76930954a156B3fF30a4E1',
@@ -34,5 +57,10 @@ export const contracts: Record<SupportedChainsIDs, ContractAddresses> = {
     setter: '0x38A4794cCEd47d3baf7370CcC43B560D3a1beEFA',
     getter: '0x352A18AEe90cdcd825d1E37d9939dCA86C00e281',
     token: '0xad45A78180961079BFaeEe349704F411dfF947C6',
+  },
+  [hoodi.id]: {
+    setter: '0x58410Bef803ECd7E63B23664C586A6DB72DAf59c',
+    getter: '0x5AdDb3f1529C5ec70D77400499eE4bbF328368fe',
+    token: '0x9F5d4Ec84fC4785788aB44F9de973cF34F7A038e',
   },
 }
