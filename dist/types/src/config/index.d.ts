3717d1e33cb59817ba77f6248ce82f36aa0da338
Ilya <ilya@blox.io>
2025-04-30 11:14:01 +0300

    chore: cleanup

--- dist/types/src/config/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './chains';
-export * from './create';
-export * from './globals';
--- dist/types/src/utils/operator.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Address } from 'viem';
-export declare const getOperatorIds: <T extends {
-    id: number;
-}[]>(operators: T) => number[];
-export declare const decodeOperatorPublicKey: (publicKey: Address) => string;
