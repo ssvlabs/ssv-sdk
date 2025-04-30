1449bd3428a72b7d079960a3d72aeac3b2e9b1fd
Ilya <ilya@blox.io>
2025-04-30 11:14:01 +0300

    chore: cleanup

--- dist/types/src/utils/contract.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ConfigReturnType } from '@/config/create';
-import type { TransactionReceipt } from 'viem';
-export declare const waitForTransaction: <T extends Promise<`0x${string}`>>(config: ConfigReturnType, fn: T) => Promise<TransactionReceipt>;
