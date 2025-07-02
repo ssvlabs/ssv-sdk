22109397c1869f31ae4d0913aa9070c7ca169365
Ilya Vilensky <ilya@ssvlabs.io>
2025-07-02 16:52:04 +0300

    feat: added hardhat/artifacts and hardhat/cache to gitignore (#25)
    * feat: added hardhat/artifacts and hardhat/cache to gitignore
    
    * ci: update artifacts [skip ci]

--- dist/types/src/contract-interactions/create.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ContractNames, ReadProps, ReaderFunctions, WriteProps, WriterFunctions } from './types';
-export declare const createWriter: <T extends ContractNames>({ abi, publicClient, walletClient, contractAddress, }: WriteProps) => WriterFunctions<T>;
-export declare const createReader: <T extends ContractNames>({ abi, publicClient, contractAddress, }: ReadProps) => ReaderFunctions<T>;
