1a06611c44b32b7adb3dc91938ea03d20b5894a9
Ilya <vilensky.ilya@gmail.com>
2025-07-03 14:49:24 +0300

    fix: changes in vite config

---
+++ dist/types/libs/ssv-keys/Threshold/Threshold.d.ts
@@ -0,0 +1,28 @@
+import { SSVKeysException } from '@/libs/ssv-keys/exceptions/base';
+import type { ISharesKeyPairs } from '@/libs/ssv-keys/interfaces';
+export declare class ThresholdInvalidOperatorsLengthError extends SSVKeysException {
+    operators: number[];
+    constructor(operators: number[], message: string);
+}
+export declare class ThresholdInvalidOperatorIdError extends SSVKeysException {
+    operator: any;
+    constructor(operator: any, message: string);
+}
+/**
+ * Building threshold for list of operator IDs
+ */
+declare class Threshold {
+    protected publicKey: any;
+    protected privateKey: any;
+    protected shares: Array<any>;
+    static get DEFAULT_THRESHOLD_NUMBER(): number;
+    /**
+     * Receives list of operators IDs.
+     *  len(operator IDs) := 3 * F + 1
+     *
+     * If F calculated from this formula is not integer number - it will raise exception.
+     * Generate keys and return promise
+     */
+    create(privateKeyString: string, operatorIds: number[]): Promise<ISharesKeyPairs>;
+}
+export default Threshold;
