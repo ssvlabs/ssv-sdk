fd9c85335f46e9273c548f206868373e713ce7b8
Ilya <vilensky.ilya@gmail.com>
2025-07-08 12:45:17 +0300

    fix: replaced eth2-keystore-js with @chainsafe/bls-keystore

---
+++ dist/types/libs/ssv-keys/KeyShares/KeySharesData/validators/owner-address.d.ts
@@ -0,0 +1,6 @@
+import type { ValidatorConstraintInterface, ValidationOptions } from 'class-validator';
+export declare class OwnerAddressValidatorConstraint implements ValidatorConstraintInterface {
+    validate(value: any): boolean;
+    defaultMessage(): string;
+}
+export declare function OwnerAddressValidator(validationOptions?: ValidationOptions): (object: any, propertyName: string) => void;
---
+++ dist/types/libs/ssv-keys/index.d.ts
@@ -0,0 +1,4 @@
+export { SSVKeys } from './SSVKeys';
+export { KeySharesItem } from './KeyShares/KeySharesItem';
+export { KeyShares } from './KeyShares/KeyShares';
+export { SSVKeysException, OperatorPublicKeyError, OperatorsCountsMismatchError } from './exceptions';
